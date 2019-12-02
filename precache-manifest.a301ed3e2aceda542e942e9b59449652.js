940b7d6d8ba17a86d8c5a953be20072b6fbf1510
artem2313 <huselnykov@gmail.com>
2019-12-02 18:58:47 +0200

    Updates

---
+++ precache-manifest.a301ed3e2aceda542e942e9b59449652.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e316c94d6a150dea8584d867bf83aea7",
+    "url": "/testReact/index.html"
+  },
+  {
+    "revision": "a501247f9cdcedd21250",
+    "url": "/testReact/static/js/2.6c9c5541.chunk.js"
+  },
+  {
+    "revision": "1d240af13a3582cf3c41",
+    "url": "/testReact/static/js/main.e1959ca7.chunk.js"
+  },
+  {
+    "revision": "dec2c23031ba8aa7da34",
+    "url": "/testReact/static/js/runtime-main.a6d77ace.js"
+  }
+]);
